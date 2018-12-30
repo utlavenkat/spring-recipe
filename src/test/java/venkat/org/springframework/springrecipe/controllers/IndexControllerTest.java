4447542289c551d4d45a67c821897ba405675e6b
Utla, Venkatasubbaiah (ES) <Venkatasubbaiah.Utla@ADP.com>
2018-12-30 21:57:47 +0530

    Written Unit tests for Index Controller using mockito. closes #3

--- src/test/java/venkat/org/springframework/springrecipe/controllers/IndexControllerTest.java
+++ src/test/java/venkat/org/springframework/springrecipe/controllers/IndexControllerTest.java
@@ -1,31 +1,57 @@
 package venkat.org.springframework.springrecipe.controllers;
 
+import lombok.val;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.ui.ConcurrentModel;
 import org.springframework.ui.Model;
 import venkat.org.springframework.springrecipe.domain.Recipe;
+import venkat.org.springframework.springrecipe.services.RecipeService;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import static org.mockito.Mockito.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class IndexControllerTest {
-    @Autowired
+
     private IndexController indexController;
 
+    @Mock
+    private RecipeService recipeService;
+
+    @Mock
+    private Model model;
+
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+        //model = new ConcurrentModel();
+
+    }
     @Test
     public void getIndexPage() {
-        Model model = new ConcurrentModel();
+        val recipe1 = new Recipe();
+        val recipe2 = new Recipe();
+        val mockedRecipeSet = new HashSet<Recipe>();
+        mockedRecipeSet.add(recipe1);
+        mockedRecipeSet.add(recipe2);
+
+        when(recipeService.getAllRecipes()).thenReturn(mockedRecipeSet);
         Assert.assertThat(indexController.getIndexPage(model), Matchers.equalToIgnoringCase("index"));
-        Set<Recipe> recipeSet = (Set<Recipe>)((ConcurrentModel) model).get("recipes");
-        Assert.assertNotNull(recipeSet);
-        Assert.assertTrue(recipeSet.size() == 2);
+        verify(recipeService,times(1)).getAllRecipes();
+
+        ArgumentCaptor<Set> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+        Assert.assertTrue(argumentCaptor.getValue().size()==2);
     }
 }
